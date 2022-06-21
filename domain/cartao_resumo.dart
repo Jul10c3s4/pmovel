1556b672c4e338b6be28f65970eabf5afeb90f40
costag1 <103394333+costag1@users.noreply.github.com>
2022-06-20 22:43:56 -0300

    Delete domain directory

--- domain/cartao_resumo.dart
+++
@@ -1,7 +0,0 @@
-class CartaoResumo {
-  final String image;
-  final String titulo;
-  final String descricao;
-
-  CartaoResumo({required this.image, required this.titulo, required this.descricao});
-}
