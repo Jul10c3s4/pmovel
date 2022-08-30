5f1bfb723537bfa103eaf3fe002668961d71fa5f
costag1 <103394333+costag1@users.noreply.github.com>
2022-08-30 07:43:28 -0300

    Update new_card.dart

--- lib/pages/new_card.dart
+++ lib/pages/new_card.dart
@@ -30,118 +30,119 @@ class _NewCardState extends State<NewCard> {
         ),
         backgroundColor: const Color(0xffa08ad7),
         body: Container(
-          padding: const EdgeInsets.all(40),
-          decoration: const BoxDecoration(
-            color: Colors.white10,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                height: 40,
-                width: double.maxFinite,
-                alignment: AlignmentDirectional.center,
-                decoration: const BoxDecoration(
-                  color: Colors.white54,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+                  padding: const EdgeInsets.all(40),
+                  decoration: const BoxDecoration(
+                    color: Colors.white10,
                   ),
-                ),
-                child: const Text('NOVO CARD', style: TextStyle(fontSize: 25)),
-              ),
-
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Título',
-                  border: UnderlineInputBorder(),
-                  prefixIconColor: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 28),
- Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'MATÉRIA',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  DropdownButton<String>(
-                      focusColor: Colors.white30,
-                      items: _materias.map((String dropDownStringItem) {
-                        return DropdownMenuItem<String>(
-                          value: dropDownStringItem,
-                          child: Text(dropDownStringItem),
-                        );
-                      }).toList(),
-                      onChanged: (String? novoItemSelecionado) {
-                        dropDownItemSelected(novoItemSelecionado!);
-                        setState(() {
-                          _itemSelecionado = novoItemSelecionado;
-                        });
-                      },
-                      value: _itemSelecionado),
-                ],
-              ),
-
-              const SizedBox(height: 16),
-
-              TextFormField(
-                keyboardType: TextInputType.multiline,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Descrição',  decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Descrição',
-                ),
-              ),
-
-              const SizedBox(height: 16),
-              //inputar link externo
-              TextFormField(
-                cursorColor: Colors.purple,
-                keyboardType: TextInputType.url,
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Url da imagem desejada',
-                ),
-              ),
-              const SizedBox(height: 35),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      primary: Colors.white,
-                      fixedSize: const Size(40, 30),
-                      backgroundColor: const Color(0xff180c36),
-                    ),
-                    onPressed: () => showDialog<String>(
-                      context: context,
-                      builder: (BuildContext context) => AlertDialog(
-                        title: const Text('Adicionar novo card'),
-                        content: const Text('Um novo card será adicionado.'),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, 'Cancel'),
-                            child: const Text('Cancel'),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: double.maxFinite,
+                        alignment: AlignmentDirectional.center,
+                        decoration: const BoxDecoration(
+                          color: Colors.white54,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
                           ),
+                        ),
+                        child:const Text('NOVO CARD',
+                            style: TextStyle(fontSize: 25)),
+                      ),
+
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Título',
+                          border: UnderlineInputBorder(),
+                          prefixIconColor: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 28),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'MATÉRIA',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          DropdownButton<String>(
+                              focusColor: Colors.white30,
+                              items: _materias.map((String dropDownStringItem) {
+                                return DropdownMenuItem<String>(
+                                  value: dropDownStringItem,
+                                  child: Text(dropDownStringItem),
+                                );
+                              }).toList(),
+                              onChanged: (String? novoItemSelecionado) {
+                                dropDownItemSelected(novoItemSelecionado!);
+                                setState(() {
+                                  _itemSelecionado = novoItemSelecionado;
+                                });
+                              },
+                              value: _itemSelecionado),
+                        ],
+                      ),
+
+                      const SizedBox(height: 16),
+
+                      TextFormField(
+                        keyboardType: TextInputType.multiline,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Descrição',
+                        ),
+                      ),
+
+                      const SizedBox(height: 16),
+                      //inputar link externo
+                      TextFormField(
+                        cursorColor: Colors.purple,
+                        keyboardType: TextInputType.url,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Url da imagem desejada',
+                        ),
+                      ),
+                      const SizedBox(height: 35),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
                           TextButton(
-                            onPressed: () => Navigator.pop(context, 'OK'),
-                            child: const Text('OK'),
+                            style: TextButton.styleFrom(
+                              primary: Colors.white,
+                              fixedSize: const Size(40, 30),
+                              backgroundColor: const Color(0xff180c36),
+                            ),
+                            onPressed: () => showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                title: const Text('Adicionar novo card'),
+                                content:
+                                const Text('Um novo card será adicionado.'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'Cancel'),
+                                    child: const Text('Cancel'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'OK'),
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            child: const Icon(Icons.check),
                           ),
                         ],
                       ),
-                    ),
-                    child: const Icon(Icons.check),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
     );
-                            
   }
 
   dropDownItemSelected(String novoItem) {
