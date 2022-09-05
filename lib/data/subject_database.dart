9ca4e00d26c292f89dd525e8d4f2925e1fc1c0cd
kelvins213 <103438311+kelvins213@users.noreply.github.com>
2022-09-05 16:40:13 -0300

    Create subject_database.dart

---
+++ lib/data/subject_database.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:untitled4/domain/conteudos_das_materias.dart';
+
+class SubjectDatabaseContents{
+
+    static Search lupa = Search("PESQUISAR", Icons.search);
+
+    static Materias portuguese = Materias("PORTUGUÊS", Icons.menu_book);
+    static Materias math = Materias("MATEMÁTICA", Icons.functions);
+    static Materias history = Materias("HISTÓRIA", Icons.hourglass_empty);
+    static Materias geografy = Materias("GEOGRAFIA", Icons.public);
+    static Materias sociology = Materias("SOCIOLOGIA", Icons.psychology);
+    static Materias philosophy = Materias("FILOSOFIA", Icons.group);
+    static Materias physical = Materias("FÍSICA", Icons.tips_and_updates);
+    static Materias chemistry = Materias("QUÍMICA", Icons.science);
+    static Materias biology = Materias("BIOLOGIA", Icons.biotech);
+    static Materias programming = Materias("PROGRAMAÇÃO", Icons.computer);
+    static Materias business = Materias("NEGÓCIOS", Icons.work);
+
+}
