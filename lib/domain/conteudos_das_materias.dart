789087296c1a013d4ace895cbe6137c5fb62490f
kelvins213 <103438311+kelvins213@users.noreply.github.com>
2022-09-05 16:44:36 -0300

    Update and rename atibutosConteudos.dart to conteudos_das_materias.dart

---
+++ lib/domain/conteudos_das_materias.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Materias {
+  final String title;
+  final IconData icon;
+
+  Materias(
+      this.title, this.icon);
+
+}
+
+class Search {
+  final String name;
+  final IconData icon;
+
+  Search(
+      this.name, this.icon);
+}
+
+class Conteudos {
+  final String titulo;
+  final String titleAppBar;
+  final IconData icon;
+  final String imagem;
+  final String conceito;
+
+  Conteudos(
+      this.titulo, this.icon, this.titleAppBar, this.imagem, this.conceito);
+}
