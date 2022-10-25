4ca6e6e3c634ca25b24b92897a2a4b99346aa3c4
TheusFL <107876710+theusFL@users.noreply.github.com>
2022-10-25 08:01:17 -0300

    Update estudos_cards.dart

--- lib/pages/meusCards/estudos_cards.dart
+++ lib/pages/meusCards/estudos_cards.dart
@@ -3,11 +3,11 @@ import 'package:app/pages/meusCards/detalhes_card.dart';
 import 'package:flutter/material.dart';
 
 class EstudosCard extends StatefulWidget {
-  final Atributos detalhe;
+  final Atributos detalhes;
 
   const EstudosCard({
     Key? key,
-    required this.detalhe,
+    required this.detalhes,
   }) : super(key: key);
 
   @override
@@ -17,53 +17,56 @@ class EstudosCard extends StatefulWidget {
 class _EstudosCardState extends State<EstudosCard> {
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) {
-              return CardDetalhes(
-                detalhes: widget.detalhe,
-              );
-            },
-          ),
-        );
-      },
-       child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 5.0),
-        padding: const EdgeInsets.all(6.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white54,
-        ),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    widget.detalhe.titulo,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    widget.detalhe.descricao,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 10,
-                    ),
-                  ),
-                ],
-              ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return CardDetalhes(
+                  detalhes: widget.detalhes,
+                );
+              },
             ),
-            const SizedBox(width: 24),
-            //buildImage(),
-          ],
+          );
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 4.0),
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.white54,
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      widget.detalhes.titulo,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      widget.detalhes.descricao,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 24),
+              //buildImage(),
+            ],
+          ),
         ),
       ),
     );
