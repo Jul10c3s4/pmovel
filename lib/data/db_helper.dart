b5af2a365111b599d5f8e6d9707ecad45ad89375
Kelvin <103438311+kelvins213@users.noreply.github.com>
2022-11-07 17:28:39 -0300

    Update db_helper.dart

--- lib/data/db_helper.dart
+++ lib/data/db_helper.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:app/data/database_contents.dart';
-import 'package:app/data/request_data.dart';
-
 
 class DB_Helper{
 
@@ -30,39 +28,7 @@ class DB_Helper{
     sqlCreateMusic = "create table DrawerGenres(id int PRIMARY KEY, genreName varchar(100), musicName varchar(60), musicSinger varchar(60), linkMusicWallpaper varchar(100), youtubeURL varchar(100))";
     await db.execute(sqlCreateMusic);
     await insertIntoTables(db);
-    
-    String sqlMusic;
-    String sqlGenre;
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (0,'Come and Go', 'https://youtu.be/SjGVvdq9ZCs', 'https://i0.wp.com/br.nacaodamusica.com/wp-content/uploads/2020/07/Juice-WRLD.jpg');";
-    await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (1,'Tipo Madara', 'https://youtu.be/wCMzj9Gzyt0?list=RDMM', 'https://images3.alphacoders.com/695/695428.jpg');";
-    await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (2,'After Party', 'https://youtu.be/GyR38LqBWEE?list=RDMM', 'https://i1.sndcdn.com/artworks-MUe0sOoezI9ORG2U-YTIzhA-t500x500.jpg');";
-    await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (3,'Feel Like God', 'https://youtu.be/Rxu-SeaLmlo?list=RDMM', 'https://i.ytimg.com/vi/kw-XWS1Usio/sddefault.jpg');";
-    await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (4,'Drunk and Nasty', 'https://youtu.be/3T5rSK0rd7k?list=RDMM', 'https://m.media-amazon.com/images/I/71iG4caogJL._SS500_.jpg');";
-    await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (5,'Vibe Zabuza', 'https://youtu.be/x6VWj8JeyIU?list=RDMM', 'https://besthqwallpapers.com/Uploads/6-12-2019/114238/thumb2-zabuza-momochi-4k-naruto-characters-momochi-zabuza-manga.jpg');";
-    await db.execute(sqlMusic);
-    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (6,'Rick and Morty', 'https://youtu.be/br8qyUSbuDI', 'https://static.itapemafm.com.br/s3fs-public/styles/itapema_blog_post_header/public/graphql-upload-files/rick%20and%20morty%20temporada%205_1.jpg');";
-    await db.execute(sqlMusic);
 
-    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (0, 'Lofi', 'Lofi Hip Hop', 'Instrumental', 'https://c4.wallpaperflare.com/wallpaper/908/34/383/lofi-digital-anthro-hd-wallpaper-preview.jpg', 'https://youtu.be/jfKfPfyJRdk')";
-    await db.execute(sqlGenre);
-    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (1, 'Hip Hop', 'Yeat - Already Rich', 'Remix', 'https://images.alphacoders.com/998/thumb-1920-998446.jpg', 'https://youtu.be/jfKfPfyJRdk')";
-    await db.execute(sqlGenre);
-    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (2, 'Pop', 'Cheerleader', 'Felix Jaehn', 'https://upload.wikimedia.org/wikipedia/commons/6/6e/JU_Cheerleaders.jpg', 'https://youtu.be/rXF9ziZ3AU4?list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbA')";
-    await db.execute(sqlGenre);
-    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (3, 'Electronic', 'Electronic', 'Instrumental', 'https://c4.wallpaperflare.com/wallpaper/908/34/383/lofi-digital-anthro-hd-wallpaper-preview.jpg', 'https://youtu.be/jfKfPfyJRdk')";
-    await db.execute(sqlGenre);
-    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (4, 'Hip Hop', 'Lucid Dreams', 'Juice WRLD', 'https://i.pinimg.com/736x/09/f8/f3/09f8f3fe8f6020afd034531d78a7637f.jpg', 'https://youtu.be/mzB1VGEGcSU?list=PLDUJWPwQe9iOVT5Y1WEL3E7kHOBTGiCvW')";
-    await db.execute(sqlGenre);
-    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (5, 'Pop', 'I Really Like to Party', 'Remix', 'https://files.musicfeed.ir/photoes/1400/08/i-really-like-to-party.jpg', 'https://youtu.be/o6jZo5ub3hw?list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbA')";
-    await db.execute(sqlGenre);
-    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (6, 'Electronic', 'Better Now', 'Post Malone', 'https://i1.sndcdn.com/artworks-000583785761-cnd2zw-t500x500.jpg', 'https://youtu.be/jfKfPfyJRdk')";
-    await db.execute(sqlGenre);
-    
     String sqlUser =
         "CREATE TABLE USER(username varchar(100) PRIMARY KEY, password varchar(100))";
     await db.execute(sqlUser);
@@ -109,18 +75,52 @@ class DB_Helper{
 
     String sql = "create table SubjectAtributes(id INTERGER PRIMARY KEY, title varchar(40), iconName varchar(40),  titleAppBar varchar(40), imageLink varchar(300), concept varchar(500))";
     await db.execute(sql);
-    await insertIntoDatas(db: db);    
+    await insertIntoDatas(db: db);
+  }
 
-    Future<FutureOr<void>> insertIntoDatas({
+  Future<FutureOr<void>> insertIntoDatas({
     required Database db,
-    }) async{
-      for (var i in subjectList) {
-        for (var j in i) {
-          dynamic json = j.toJson();
-          await db.insert('SubjectAtributes', json);
-        }
+  }) async{
+    for (var i in subjectList) {
+      for (var j in i) {
+        dynamic json = j.toJson();
+        await db.insert('SubjectAtributes', json);
       }
     }
+  }
+
+  Future<FutureOr<void>> insertIntoTables(Database db) async {
+    String sqlMusic;
+    String sqlGenre;
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (0,'Come and Go', 'https://youtu.be/SjGVvdq9ZCs', 'https://i0.wp.com/br.nacaodamusica.com/wp-content/uploads/2020/07/Juice-WRLD.jpg');";
+    await db.execute(sqlMusic);
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (1,'Tipo Madara', 'https://youtu.be/wCMzj9Gzyt0?list=RDMM', 'https://images3.alphacoders.com/695/695428.jpg');";
+    await db.execute(sqlMusic);
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (2,'After Party', 'https://youtu.be/GyR38LqBWEE?list=RDMM', 'https://i1.sndcdn.com/artworks-MUe0sOoezI9ORG2U-YTIzhA-t500x500.jpg');";
+    await db.execute(sqlMusic);
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (3,'Feel Like God', 'https://youtu.be/Rxu-SeaLmlo?list=RDMM', 'https://i.ytimg.com/vi/kw-XWS1Usio/sddefault.jpg');";
+    await db.execute(sqlMusic);
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (4,'Drunk and Nasty', 'https://youtu.be/3T5rSK0rd7k?list=RDMM', 'https://m.media-amazon.com/images/I/71iG4caogJL._SS500_.jpg');";
+    await db.execute(sqlMusic);
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (5,'Vibe Zabuza', 'https://youtu.be/x6VWj8JeyIU?list=RDMM', 'https://besthqwallpapers.com/Uploads/6-12-2019/114238/thumb2-zabuza-momochi-4k-naruto-characters-momochi-zabuza-manga.jpg');";
+    await db.execute(sqlMusic);
+    sqlMusic = "insert into StudyMusics(id, name, link, imageLink) values (6,'Rick and Morty', 'https://youtu.be/br8qyUSbuDI', 'https://static.itapemafm.com.br/s3fs-public/styles/itapema_blog_post_header/public/graphql-upload-files/rick%20and%20morty%20temporada%205_1.jpg');";
+    await db.execute(sqlMusic);
+
+    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (0, 'Lofi', 'Lofi Hip Hop', 'Instrumental', 'https://c4.wallpaperflare.com/wallpaper/908/34/383/lofi-digital-anthro-hd-wallpaper-preview.jpg', 'https://youtu.be/jfKfPfyJRdk')";
+    await db.execute(sqlGenre);
+    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (1, 'Hip Hop', 'Yeat - Already Rich', 'Remix', 'https://images.alphacoders.com/998/thumb-1920-998446.jpg', 'https://youtu.be/jfKfPfyJRdk')";
+    await db.execute(sqlGenre);
+    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (2, 'Pop', 'Cheerleader', 'Felix Jaehn', 'https://upload.wikimedia.org/wikipedia/commons/6/6e/JU_Cheerleaders.jpg', 'https://youtu.be/rXF9ziZ3AU4?list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbA')";
+    await db.execute(sqlGenre);
+    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (3, 'Electronic', 'Electronic', 'Instrumental', 'https://c4.wallpaperflare.com/wallpaper/908/34/383/lofi-digital-anthro-hd-wallpaper-preview.jpg', 'https://youtu.be/jfKfPfyJRdk')";
+    await db.execute(sqlGenre);
+    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (4, 'Hip Hop', 'Lucid Dreams', 'Juice WRLD', 'https://i.pinimg.com/736x/09/f8/f3/09f8f3fe8f6020afd034531d78a7637f.jpg', 'https://youtu.be/mzB1VGEGcSU?list=PLDUJWPwQe9iOVT5Y1WEL3E7kHOBTGiCvW')";
+    await db.execute(sqlGenre);
+    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (5, 'Pop', 'I Really Like to Party', 'Remix', 'https://files.musicfeed.ir/photoes/1400/08/i-really-like-to-party.jpg', 'https://youtu.be/o6jZo5ub3hw?list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbA')";
+    await db.execute(sqlGenre);
+    sqlGenre = "insert into DrawerGenres(id, genreName, musicName, musicSinger, linkMusicWallpaper, youtubeURL) values (6, 'Electronic', 'Better Now', 'Post Malone', 'https://i1.sndcdn.com/artworks-000583785761-cnd2zw-t500x500.jpg', 'https://youtu.be/jfKfPfyJRdk')";
+    await db.execute(sqlGenre);
   }
 
   Future<void> onUpgrade(Database db, int oldVersion, int newVersion) async {
