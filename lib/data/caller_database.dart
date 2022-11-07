31c97e45b83d84aab7cd6505f91deddff14d9b14
Kelvin <103438311+kelvins213@users.noreply.github.com>
2022-11-07 17:29:35 -0300

    Update caller_database.dart

--- lib/data/caller_database.dart
+++ lib/data/caller_database.dart
@@ -1,10 +1,11 @@
 import 'package:sqflite/sqlite_api.dart';
-import 'package:untitled4/data/DBHelper.dart';
+
+import 'db_helper.dart';
 
 class CallDatabase{
 
  Future<void> buildDatabase() async{
-   Database db = await DBHelper().initDB();
+   Database db = await DB_Helper().initDB();
    String sql = "select * from SubjectAtributes";
    final json = await db.rawQuery(sql);
    print(json);
